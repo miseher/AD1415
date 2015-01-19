3a9e9c4489ea7cb440e0dfbaf02e3941fc137f65
miseher <erjaxondo1990@hotmail.com>
2015-01-19 10:40:28 +0100

    hibernate mas completo

--- JHibernateCategorias/src/main/java/serpis/ad/Categoria.java
+++ JHibernateCategorias/src/main/java/serpis/ad/Categoria.java
@@ -2,17 +2,24 @@ package serpis.ad;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Id;
 
+@Entity 
 public class Categoria {
     private Long id;
     private String nombre;
 	
+public Categoria(){}    
+  
     
+@Id    
+@GeneratedValue(generator="increment")
+@GenericGenerator(name="increment", strategy = "increment")
     public Long getId() {
 		return id;
     }
@@ -21,16 +28,14 @@ public class Categoria {
 		this.id = id;
     }
 	
-    public void setDate(Date date) {
-  		this.date = date;
+  
+
+      public String getNombre() {
+  		return nombre;
       }
 
-      public String getTitle() {
-  		return title;
-      }
-
-      public void setTitle(String title) {
-  		this.title = title;
+      public void setNombre(String nombre) {
+  		this.nombre = nombre;
       } 
     
     
